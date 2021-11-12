98f190142a8f1f079cb44b2809d236cde7d2b35b
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-11-12 11:06:11 -0600

    Update main.js

--- bots/discord/main.js
+++ bots/discord/main.js
@@ -10,6 +10,19 @@ client.on("message", msg => {
   if (msg.content ===  PREFIX + "ping") {
     msg.reply("Pong!");
   }
+  if (msg.content === PREFIX + "minecraft") {
+    var url = "https://api.minetools.eu/ping/dove11992.aternos.me/25565";
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if ((request.responseText).indexOf("\\u00a74Server not found.") != -1) {
+          msg.reply("Server Offline.");
+        } else {
+          msg.reply("Server Online.");
+      }};
+    xhr.send();
+  }
 })
 
 client.login(process.env.TOKEN)
