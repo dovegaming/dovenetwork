45e01e2721877db3fef78e8c5d70c0d9dd264dfa
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-11-15 09:18:39 -0600

    Update main.py

--- runtime/bots/irc/main.py
+++ runtime/bots/irc/main.py
@@ -48,17 +48,14 @@ while 1:
             html = requests.get("https://www.youtube.com/results?search_query=" + " ".join(msg.split()[1:])).content
             video_ids = re.findall(r"watch\?v=(\S{11})", html.decode())
             send("PRIVMSG {} :https://www.youtube.com/watch?v={}\r\n".format(CHANNEL, video_ids[0]).encode("utf-8"))
-        #if msg.startswith("$google "): send("PRIVMSG {} :{}\r\n".format(CHANNEL, googlesearch.search(" ".join(msg.split()[1:]))[0]).encode("utf-8"))
-        #if msg.startswith("$wolfram "): send("PRIVMSG {} :{}\r\n".format(CHANNEL, wolfram.get(" ".join(msg.split()[1:]))).encode("utf-8"))
         if msg.startswith("$github "):
             if tokens[1] == "url": send("PRIVMSG {} :https://github.com/{}/{}\r\n".format(CHANNEL, tokens[2], tokens[3]).encode("utf-8"))
             if tokens[1] == "issues": send("PRIVMSG {} :#{}: {}\r\n".format(CHANNEL, tokens[4], github.get_issue_title(tokens[2], tokens[3], tokens[4])).encode("utf-8"))
         if msg == "$server": send("PRIVMSG {} :{}\r\n".format(CHANNEL, minecraft.get()).encode("utf-8"))
-        if msg == "$help": send("PRIVMSG {} :Avalible commands: $hello, $ping, $youtube, $google, $github, $wolfram.\r\n".format(CHANNEL).encode("utf-8"))
+        if msg == "$help": send("PRIVMSG {} :Avalible commands: $hello, $ping, $youtube, $github.\r\n".format(CHANNEL).encode("utf-8"))
         if msg.startswith("$help "):
             if tokens[1] == "hello": send("PRIVMSG {} :Syntax: $hello  Action: Says \"Hello!\".\r\n".format(CHANNEL).encode("utf-8"))
             if tokens[1] == "ping":send("PRIVMSG {} :Syntax: $ping  Action: Says \"Ping!\".\r\n".format(CHANNEL).encode("utf-8"))
             if tokens[1] == "youtube": send("PRIVMSG {} :Syntax: $youtube <keyword> Action: Sends the URL of a YouTube video matching the keyword given.\r\n".format(CHANNEL).encode("utf-8"))
-            #if tokens[1] == "google": send("PRIVMSG {} :Syntax: $google <keyword> Action: Sends the URL of a google search with the keyword given\r\n".format(CHANNEL).encode("utf-8"))
             if tokens[1] == "github": send("PRIVMSG {} :Syntax: $github <topic> <user> <repo> <number> Action: Returns data about a github repo.\r\n".format(CHANNEL).encode("utf-8"))
-            #if tokens[1] == "wolfram": send("PRIVMSG {} :Syntax: $wolfram <query> Action: Asks Wolfram|Alpha the query given.\r\n".format(CHANNEL).encode("utf-8"))
+            if tokens[1] == "server": send("PRIVMSG {} :Syntax: $server  Action: Gets the status of Dove's Minecraft Server.\r\n".format(CHANNEL).encode("utf-8"))
